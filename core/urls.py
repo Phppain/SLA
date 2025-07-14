656393d7a166dbd69fdf176fa28e937e33768949
Azizali <azizali@azizali.local>
2025-07-14 16:05:16 +0500

    Added urls for new views

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,22 @@ from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from api.views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 router = DefaultRouter()
 router.register(r'Users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),      # обновление access токена
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),         # проверка токена
+    path('api/logout/', LogoutView.as_view(), name='logout'),
+    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/activate/<uidb64>/<token>/', ActivateAccountView.as_view(), name='activate'),
 ]
